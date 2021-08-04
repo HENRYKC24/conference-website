42bc8559f0b7cea3ad6b0b5bb870ba16a48f53c1
HENRY-KC <henrykc24@yahoo.com>
2021-08-04 11:06:31 +0100

    Remove console.log statement

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -5,7 +5,6 @@ menuButton.classList.replace('fa-bars', 'fa-times');
 menuButton.classList.replace('fa-times', 'fa-bars');
 
 const showMobileMenu = () => {
-  console.log(navbar);
   navbar.classList.toggle('show');
   menuButton.classList.toggle('fa-bars');
   menuButton.classList.toggle('fa-times');
