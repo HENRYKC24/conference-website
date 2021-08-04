daeed344aff360cb1cefad51d40cb9bbf24597c3
HENRY-KC <henrykc24@yahoo.com>
2021-08-04 12:48:15 +0100

    Finish up mobile menu

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -3,8 +3,11 @@ const menuButton = document.querySelector('.fa-bars');
 const navbar = document.querySelector('.navbar');
 menuButton.classList.replace('fa-bars', 'fa-times');
 menuButton.classList.replace('fa-times', 'fa-bars');
+const body = document.querySelector('body');
+body.setAttribute('class', '');
 
 const showMobileMenu = () => {
+  body.classList.toggle('stop-scrolling');
   navbar.classList.toggle('show');
   menuButton.classList.toggle('fa-bars');
   menuButton.classList.toggle('fa-times');
