ccaf1cda74a024062fc342af71e248dcfc2e7a95
HENRY-KC <henrykc24@yahoo.com>
2021-08-04 12:57:07 +0100

    Fix navbar issues

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,4 +1,5 @@
 // Mobile Menu
+
 const menuButton = document.querySelector('.fa-bars');
 const navbar = document.querySelector('.navbar');
 menuButton.classList.replace('fa-bars', 'fa-times');
@@ -7,6 +8,7 @@ const body = document.querySelector('body');
 body.setAttribute('class', '');
 
 const showMobileMenu = () => {
+  document.querySelector('.navlist').classList.toggle('expand');
   body.classList.toggle('stop-scrolling');
   navbar.classList.toggle('show');
   menuButton.classList.toggle('fa-bars');
